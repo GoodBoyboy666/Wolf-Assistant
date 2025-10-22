b1b8d3eb6b05ed0aae357af23b4572c932053ec9
GoodBoyboy <me@goodboyboy.top>
2025-10-22 22:36:34 +0800

    💄 style: 代码格式调整
    - 代码格式调整

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,7 +62,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlin{
+    kotlin {
         compilerOptions {
             jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
         }
