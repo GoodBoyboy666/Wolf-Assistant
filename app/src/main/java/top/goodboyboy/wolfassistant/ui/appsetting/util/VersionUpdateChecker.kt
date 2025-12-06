11c28823eb0755ef8067c27f4564964a516ccd23
GoodBoyboy <me@goodboyboy.top>
2025-12-07 00:30:04 +0800

    解决一行过长问题

--- app/src/main/java/top/goodboyboy/wolfassistant/ui/appsetting/util/VersionUpdateChecker.kt
+++ app/src/main/java/top/goodboyboy/wolfassistant/ui/appsetting/util/VersionUpdateChecker.kt
@@ -10,23 +10,24 @@ class VersionUpdateChecker
         private val gitHubDataSource: GitHubDataSource,
     ) {
         suspend fun checkUpdate(oldVersionName: String): VersionDomainData {
-            val oldVersionNameItem = VersionUtil.getVersionNameItem(oldVersionName.removePrefix("v"))
+            val oldItem = VersionUtil.getVersionNameItem(oldVersionName.removePrefix("v"))
             when (val latestVersionResult = gitHubDataSource.checkUpdateInfo()) {
                 is GitHubDataSource.VersionDataResult.Error -> {
                     return VersionDomainData.Error(latestVersionResult.error)
                 }
 
                 is GitHubDataSource.VersionDataResult.Success -> {
-                    val latestVersionNameItem = latestVersionResult.data.versionNameItem
+                    val latestItem = latestVersionResult.data.versionNameItem
 
-                    val isNewer = when {
-                        latestVersionNameItem.majorVersionNumber > oldVersionNameItem.majorVersionNumber -> true
-                        latestVersionNameItem.majorVersionNumber < oldVersionNameItem.majorVersionNumber -> false
-                        latestVersionNameItem.secondaryVersionNumber > oldVersionNameItem.secondaryVersionNumber -> true
-                        latestVersionNameItem.secondaryVersionNumber < oldVersionNameItem.secondaryVersionNumber -> false
-                        latestVersionNameItem.revisionVersionNumber > oldVersionNameItem.revisionVersionNumber -> true
-                        else -> false
-                    }
+                    val isNewer =
+                        when {
+                            latestItem.majorVersionNumber > oldItem.majorVersionNumber -> true
+                            latestItem.majorVersionNumber < oldItem.majorVersionNumber -> false
+                            latestItem.secondaryVersionNumber > oldItem.secondaryVersionNumber -> true
+                            latestItem.secondaryVersionNumber < oldItem.secondaryVersionNumber -> false
+                            latestItem.revisionVersionNumber > oldItem.revisionVersionNumber -> true
+                            else -> false
+                        }
 
                     return if (isNewer) {
                         VersionDomainData.Success(latestVersionResult.data)
