b94308a3e543cc423e33a24a0cd156312fd7500e
GoodBoyboy <me@goodboyboy.top>
2025-12-13 12:44:38 +0000

    🐛 Bug 修复 (Bugfix): 修复开启扫一扫后掉帧问题 (#31)
    * 修复开启扫一扫后掉帧问题
    
    * 发布1.3.0-beta.2版

--- app/src/main/java/top/goodboyboy/wolfassistant/ui/sanner/CameraPreview.kt
+++ app/src/main/java/top/goodboyboy/wolfassistant/ui/sanner/CameraPreview.kt
@@ -8,14 +8,13 @@ import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.compose.LocalLifecycleOwner
 
 @Composable
 fun CameraPreview(onResult: (String) -> Unit) {
-    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
     AndroidView(
         factory = { ctx ->
             val previewView = PreviewView(ctx)
@@ -36,7 +35,7 @@ fun CameraPreview(onResult: (String) -> Unit) {
                 val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
                 cameraProvider.unbindAll()
                 cameraProvider.bindToLifecycle(
-                    ctx as LifecycleOwner,
+                    lifecycleOwner,
                     cameraSelector,
                     preview,
                     analyzer,
