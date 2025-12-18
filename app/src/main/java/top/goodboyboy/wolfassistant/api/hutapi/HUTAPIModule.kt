6a5dc768bd92b03ef7df25e00cc7c066dbff6092
GoodBoyboy <me@goodboyboy.top>
2025-12-18 13:16:56 +0800

    修改OKHttpClient初始化逻辑
    解决Cookie多线程共享问题

--- app/src/main/java/top/goodboyboy/wolfassistant/api/hutapi/HUTAPIModule.kt
+++ app/src/main/java/top/goodboyboy/wolfassistant/api/hutapi/HUTAPIModule.kt
@@ -1,16 +1,16 @@
 package top.goodboyboy.wolfassistant.api.hutapi
 
 import android.annotation.SuppressLint
+import android.util.Log
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import okhttp3.ConnectionSpec
+import okhttp3.ConnectionPool
 import okhttp3.CookieJar
 import okhttp3.Dns
 import okhttp3.OkHttpClient
-import okhttp3.Protocol
-import okhttp3.TlsVersion
+import okhttp3.internal.tls.OkHostnameVerifier
 import retrofit2.Retrofit
 import top.goodboyboy.wolfassistant.api.hutapi.message.MessageAPIService
 import top.goodboyboy.wolfassistant.api.hutapi.portal.PortalAPIService
@@ -22,12 +22,15 @@ import top.goodboyboy.wolfassistant.api.hutapi.user.LoginAPIService
 import top.goodboyboy.wolfassistant.api.hutapi.user.UserAPIService
 import top.goodboyboy.wolfassistant.ui.appsetting.GlobalInitConfig
 import java.net.Inet4Address
+import java.security.KeyStore
 import java.security.SecureRandom
 import java.security.cert.X509Certificate
+import java.util.concurrent.TimeUnit
 import javax.inject.Named
 import javax.inject.Singleton
+import javax.net.ssl.HostnameVerifier
 import javax.net.ssl.SSLContext
-import javax.net.ssl.TrustManager
+import javax.net.ssl.TrustManagerFactory
 import javax.net.ssl.X509TrustManager
 
 @Module
@@ -35,66 +38,82 @@ import javax.net.ssl.X509TrustManager
 object HUTAPIModule {
     @Provides
     @Singleton
-    fun provideOkHttpClient(cookieJar: CookieJar): OkHttpClient {
-        val disableSSLCertVerification = GlobalInitConfig.disableSSL
-        val onlyIPv4 = GlobalInitConfig.onlyIPv4
+    fun provideSharedConnectionPool(): ConnectionPool = ConnectionPool(5, 1, TimeUnit.MINUTES)
 
-        // 禁用SSL配置
-        val trustAllCerts =
-            arrayOf<TrustManager>(
-                @SuppressLint("CustomX509TrustManager")
-                object : X509TrustManager {
-                    @SuppressLint("TrustAllX509TrustManager")
-                    override fun checkClientTrusted(
-                        chain: Array<out X509Certificate>?,
-                        authType: String?,
-                    ) {}
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(okHttpBuilder: OkHttpClient.Builder): OkHttpClient = okHttpBuilder.build()
 
-                    @SuppressLint("TrustAllX509TrustManager")
-                    override fun checkServerTrusted(
-                        chain: Array<out X509Certificate>?,
-                        authType: String?,
-                    ) {}
+    @Provides
+    fun provideOKHttpBuilder(
+        cookieJar: CookieJar,
+        sharedPool: ConnectionPool,
+    ): OkHttpClient.Builder {
+        val trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
+        trustManagerFactory.init(null as KeyStore?)
+        val systemDefaultTrustManager =
+            trustManagerFactory.trustManagers.first {
+                it is X509TrustManager
+            } as X509TrustManager
 
-                    override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
-                },
-            )
-//            val loggingInterceptor = HttpLoggingInterceptor().apply {
-//                level = HttpLoggingInterceptor.Level.BODY // 打印请求详情用于调试
-//            }
+        val dynamicTrustManager =
+            @SuppressLint("CustomX509TrustManager")
+            object : X509TrustManager {
+                override fun checkClientTrusted(
+                    chain: Array<out X509Certificate>?,
+                    authType: String?,
+                ) {
+                    systemDefaultTrustManager.checkClientTrusted(chain, authType)
+                }
 
-        val permissiveConnectionSpec =
-            ConnectionSpec
-                .Builder(ConnectionSpec.COMPATIBLE_TLS)
-                .tlsVersions(TlsVersion.TLS_1_2, TlsVersion.TLS_1_1, TlsVersion.TLS_1_0)
-                .allEnabledCipherSuites()
-                .build()
-        val sslContext = SSLContext.getInstance("TLS")
-        sslContext.init(null, trustAllCerts, SecureRandom())
-        val sslSocketFactory = sslContext.socketFactory
+                override fun checkServerTrusted(
+                    chain: Array<out X509Certificate>?,
+                    authType: String?,
+                ) {
+                    // 判断是否禁用SSL验证
+                    if (GlobalInitConfig.disableSSL) {
+                        Log.w("SSL_Warning", "Ignoring server certificate validation")
+                        return
+                    } else {
+                        Log.i("SSL_Info", "执行系统标准校验")
+                        systemDefaultTrustManager.checkServerTrusted(chain, authType)
+                    }
+                }
 
-        // 解决v6下服务器返回502问题（难蚌，真会折腾人）
-        val ipv4OnlyDns =
-            Dns { hostname ->
-                Dns.SYSTEM.lookup(hostname).filter { it is Inet4Address }
+                override fun getAcceptedIssuers(): Array<X509Certificate> = systemDefaultTrustManager.acceptedIssuers
             }
 
-        val client =
+        val sslContext = SSLContext.getInstance("TLS")
+        sslContext.init(null, arrayOf(dynamicTrustManager), SecureRandom())
+
+        val dynamicHostnameVerifier =
+            HostnameVerifier { hostname, session ->
+                // 判断是否禁用主机名验证
+                if (GlobalInitConfig.disableSSL) {
+                    true
+                } else {
+                    OkHostnameVerifier.verify(hostname, session)
+                }
+            }
+
+        // 解决v6下服务器返回502问题（难蚌，真会折腾人）
+        val dynamicIpv4Only =
+            Dns { hostname ->
+                if (GlobalInitConfig.onlyIPv4) {
+                    Dns.SYSTEM.lookup(hostname).filter { it is Inet4Address }
+                } else {
+                    Dns.SYSTEM.lookup(hostname)
+                }
+            }
+        val builder =
             OkHttpClient
                 .Builder()
                 .cookieJar(cookieJar)
-
-        if (disableSSLCertVerification) {
-            client
-                .sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
-                .protocols(listOf(Protocol.HTTP_1_1))
-                .hostnameVerifier { _, _ -> true }
-                .connectionSpecs(listOf(permissiveConnectionSpec, ConnectionSpec.CLEARTEXT)) // 应用宽容的加密套件
-        }
-        if (onlyIPv4) {
-            client.dns(ipv4OnlyDns)
-        }
-        return client.build()
+                .connectionPool(sharedPool)
+                .sslSocketFactory(sslContext.socketFactory, dynamicTrustManager)
+                .hostnameVerifier(dynamicHostnameVerifier)
+                .dns(dynamicIpv4Only)
+        return builder
     }
 
     @Provides
@@ -186,94 +205,39 @@ object HUTAPIModule {
     @Provides
     @Singleton
     @Named("LabSchedule")
-    fun provideLabScheduleOkHttpClient(cookieJar: CookieJar): OkHttpClient {
-        val disableSSLCertVerification = GlobalInitConfig.disableSSL
-        val onlyIPv4 = GlobalInitConfig.onlyIPv4
-
-        // 禁用SSL配置
-        val trustAllCerts =
-            arrayOf<TrustManager>(
-                @SuppressLint("CustomX509TrustManager")
-                object : X509TrustManager {
-                    @SuppressLint("TrustAllX509TrustManager")
-                    override fun checkClientTrusted(
-                        chain: Array<out X509Certificate>?,
-                        authType: String?,
-                    ) {
-                    }
-
-                    @SuppressLint("TrustAllX509TrustManager")
-                    override fun checkServerTrusted(
-                        chain: Array<out X509Certificate>?,
-                        authType: String?,
-                    ) {
-                    }
-
-                    override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
-                },
-            )
-
-        val permissiveConnectionSpec =
-            ConnectionSpec
-                .Builder(ConnectionSpec.COMPATIBLE_TLS)
-                .tlsVersions(TlsVersion.TLS_1_2, TlsVersion.TLS_1_1, TlsVersion.TLS_1_0)
-                .allEnabledCipherSuites()
-                .build()
-        val sslContext = SSLContext.getInstance("TLS")
-        sslContext.init(null, trustAllCerts, SecureRandom())
-        val sslSocketFactory = sslContext.socketFactory
-
-        // 禁用IPv6配置
-        val ipv4OnlyDns =
-            Dns { hostname ->
-                Dns.SYSTEM.lookup(hostname).filter { it is Inet4Address }
-            }
-
+    fun provideLabScheduleOkHttpClient(okHttpBuilder: OkHttpClient.Builder): OkHttpClient {
         // 构建
-        val client =
-            OkHttpClient
-                .Builder()
-                .cookieJar(cookieJar)
-                .followRedirects(true)
-                .addNetworkInterceptor { chain ->
-                    val original = chain.request()
-                    val requestBuilder =
-                        original
-                            .newBuilder()
-                            .header(
-                                "User-Agent",
-                                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36 Edg/143.0.0.0",
-                            ).header(
-                                "Accept",
-                                "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-                            ).header("Accept-Language", "zh-CN,zh;q=0.9")
-                            .header("Upgrade-Insecure-Requests", "1")
-                            .header("Connection", "keep-alive")
-                            .header("Cache-Control", "no-cache")
-                            .header("Pragma", "no-cache")
-                            .header("DNT", "1")
+        okHttpBuilder
+            .followRedirects(true)
+            .addNetworkInterceptor { chain ->
+                val original = chain.request()
+                val requestBuilder =
+                    original
+                        .newBuilder()
+                        .header(
+                            "User-Agent",
+                            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36 Edg/143.0.0.0",
+                        ).header(
+                            "Accept",
+                            "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                        ).header("Accept-Language", "zh-CN,zh;q=0.9")
+                        .header("Upgrade-Insecure-Requests", "1")
+                        .header("Connection", "keep-alive")
+                        .header("Cache-Control", "no-cache")
+                        .header("Pragma", "no-cache")
+                        .header("DNT", "1")
 
-                    // 手动伪造 Referer
-                    // 虽然浏览器从 HTTPS 跳到 HTTP 会丢弃 Referer，但手动补上通常能修复很多弱智后端 Bug
-                    // 只有当请求的是 jwxt 时才加，避免污染其他请求
-                    if (original.url.host.contains("jwxt")) {
-                        requestBuilder.header("Referer", "https://mycas.hut.edu.cn/")
-                    }
-
-                    chain.proceed(requestBuilder.build())
+                // 手动伪造 Referer
+                // 虽然浏览器从 HTTPS 跳到 HTTP 会丢弃 Referer，但手动补上通常能修复很多弱智后端 Bug
+                // 只有当请求的是 jwxt 时才加，避免污染其他请求
+                if (original.url.host.contains("jwxt")) {
+                    requestBuilder.header("Referer", "https://mycas.hut.edu.cn/")
                 }
+
+                chain.proceed(requestBuilder.build())
+            }
 //            .addNetworkInterceptor(HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.HEADERS })
-        if (disableSSLCertVerification) {
-            client
-                .sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
-                .protocols(listOf(Protocol.HTTP_1_1))
-                .hostnameVerifier { _, _ -> true }
-                .connectionSpecs(listOf(permissiveConnectionSpec, ConnectionSpec.CLEARTEXT)) // 应用宽容的加密套件
-        }
-        if (onlyIPv4) {
-            client.dns(ipv4OnlyDns)
-        }
-        return client.build()
+        return okHttpBuilder.build()
     }
 
     @Provides
