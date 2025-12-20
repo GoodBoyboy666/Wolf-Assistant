6164c1fc0b247c7f22484b3fbad89bdd695088bd
GoodBoyboy <me@goodboyboy.top>
2025-12-20 20:32:55 +0800

    修订版本至1.4.0-beta.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "top.goodboyboy.wolfassistant"
         minSdk = 28
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.3.0"
+        versionCode = 12
+        versionName = "1.4.0-beta.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         android.buildFeatures.buildConfig = true
