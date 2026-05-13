c400db0761de3f6f1d8c4a4fc6f81de7739c8556
GoodBoyboy <me@goodboyboy.top>
2026-05-13 22:11:06 +0800

    deps: 更新依赖 (#77)
    * deps: 更新依赖
    
    * fix: 解决单元测试问题

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("com.google.devtools.ksp")
     id("com.google.dagger.hilt.android")
@@ -119,6 +118,7 @@ dependencies {
     implementation(libs.androidx.compose.material3.window.size.class1)
     testImplementation(libs.junit)
     testImplementation(libs.junit.jupiter)
+    testRuntimeOnly(libs.junit.platform.launcher)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
